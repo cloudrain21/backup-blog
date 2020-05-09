c79d53a3bb84240ac40762965a9a3cee45ca9dde
rain.i <rain.i@kakaoenterprise.com>
2020-05-10 06:32:46 +0900

    commander

--- module/commander/ssh.go
+++ module/commander/ssh.go
@@ -1,19 +1,39 @@
 package commander
 
+import (
+	"errors"
+	"github.com/sfreiberg/simplessh"
+	"log"
+)
+
 type SSH struct {
 	url  string
 	user string
 	pass string
+	conn *simplessh.Client
 }
 
+var (
+	ErrSSHConnect = errors.New("ssh connection error")
+)
+
 func (s *SSH) Connect() error {
+	log.Println("connect...")
+
+	client, err := simplessh.ConnectWithPassword(s.url, s.user, s.pass)
+	if err != nil {
+		return ErrSSHConnect
+	}
+
+	s.conn = client
 	return nil
 }
 
 func (s SSH) RunCommand(command string) error {
+	log.Printf("run command : %s\n", command)
 	return nil
 }
 
 func NewSSH(url string, user string, pass string) *SSH {
-	return &SSH{url, user, pass}
+	return &SSH{url, user, pass, nil}
 }
